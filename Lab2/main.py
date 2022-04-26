2bc6264689eab8d95d3ebb7b8ea1fc1557be0620
NaN <blackpaper1993@gmail.com>
2022-04-26 17:29:48 +0200

    removed multiprocess code, since at the moment is not used, small cleanup of code

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,10 +2,10 @@ from data_structures.graph import Graph
 import algorithms as algorithms
 
 from time import perf_counter_ns
-import gc, os
+import gc
+import os
 import pandas as pd
 import numpy as np
-from multiprocessing import Process
 
 
 def measureTime(Graphs, Function, MSTs, Time):
@@ -37,13 +37,7 @@ def main():
         Graphs_names.append(filename)
         print(filename, "\t=>\tOK")
 
-    # MSTs_Weights_Prim = []
-    # MSTs_Weights_Kruskal = []
-    # MSTs_Weights_Kruscal_Efficient = []
-    # run_times_Prim = []
-    # run_times_Kruskal_Efficient = []
-    # run_times_Kruskal = []
-
+    # no calls to the run algorithms for the moment
     # Run the algorithms and computes all the measurements
     # param Multiprocess = True/False
     # run(
@@ -54,7 +48,6 @@ def main():
     #    run_times_Prim,
     #    run_times_Kruskal,
     #    run_times_Kruskal_Efficient,
-    #    False,
     # )
 
     # graph_data = []  # Obtaining data for the graphs references
@@ -72,122 +65,18 @@ def run(
     run_times_Prim,
     run_times_Kruskal,
     run_times_Kruskal_Efficient,
-    Multiprocess=False,
 ):
     # WARNING Don't let Kruskal run on the entire graph dataset. It takes too long.
 
-    if Multiprocess is False:
-        ############## No processs ##############
-        measureTime(Graphs, algorithms.Prim_Heap, MSTs_Weights_Prim, run_times_Prim)
-        measureTime(Graphs, algorithms.Kruskal, MSTs_Weights_Kruskal, run_times_Kruskal)
-        measureTime(
-            Graphs,
-            algorithms.Efficient_Kruskal,
-            MSTs_Weights_Kruscal_Efficient,
-            run_times_Kruskal_Efficient,
-        )
-        ########################################
-    else:
-        ############## processs ################
-        MSTs_Weights_Kruskal_1 = []
-        MSTs_Weights_Kruskal_2 = []
-        MSTs_Weights_Kruskal_3 = []
-        MSTs_Weights_Kruskal_4 = []
-        MSTs_Weights_Kruskal_5 = []
-        run_times_Kruskal_1 = []  # Kruskal on 1-39  graphs
-        run_times_Kruskal_2 = []  # Kruskal on 40-52 graphs
-        run_times_Kruskal_3 = []  # Kruskal on 53-59 graphs
-        run_times_Kruskal_4 = []  # Kruskal on 60-64 graphs
-        run_times_Kruskal_5 = []  # Kruskal on 65-68 graphs
+    measureTime(Graphs, algorithms.Prim_Heap, MSTs_Weights_Prim, run_times_Prim)
+    measureTime(Graphs, algorithms.Kruskal, MSTs_Weights_Kruskal, run_times_Kruskal)
+    measureTime(
+        Graphs,
+        algorithms.Efficient_Kruskal,
+        MSTs_Weights_Kruscal_Efficient,
+        run_times_Kruskal_Efficient,
+    )
 
-        Prim_process = Process(
-            target=measureTime,
-            args=(Graphs, algorithms.Prim_Heap, MSTs_Weights_Prim, run_times_Prim),
-        )
-
-        Kruskal_process_1 = Process(
-            target=measureTime,
-            args=(
-                Graphs[:40],
-                algorithms.Kruskal,
-                MSTs_Weights_Kruskal_1,
-                run_times_Kruskal_1,
-            ),
-        )
-        Kruskal_process_2 = Process(
-            target=measureTime,
-            args=(
-                Graphs[40:53],
-                algorithms.Kruskal,
-                MSTs_Weights_Kruskal_2,
-                run_times_Kruskal_2,
-            ),
-        )
-        Kruskal_process_3 = Process(
-            target=measureTime,
-            args=(
-                Graphs[53:60],
-                algorithms.Kruskal,
-                MSTs_Weights_Kruskal_3,
-                run_times_Kruskal_3,
-            ),
-        )
-        Kruskal_process_4 = Process(
-            target=measureTime,
-            args=(
-                Graphs[60:65],
-                algorithms.Kruskal,
-                MSTs_Weights_Kruskal_4,
-                run_times_Kruskal_4,
-            ),
-        )
-        Kruskal_process_5 = Process(
-            target=measureTime,
-            args=(
-                Graphs[65:],
-                algorithms.Kruskal,
-                MSTs_Weights_Kruskal_5,
-                run_times_Kruskal_5,
-            ),
-        )
-        Kruskal_Efficient_process = Process(
-            target=measureTime,
-            args=(
-                Graphs,
-                algorithms.Efficient_Kruskal,
-                MSTs_Weights_Kruscal_Efficient,
-                run_times_Kruskal_Efficient,
-            ),
-        )
-        process = [
-            Prim_process,
-            Kruskal_process_1,
-            Kruskal_process_2,
-            Kruskal_process_3,
-            Kruskal_process_4,
-            Kruskal_process_5,
-            Kruskal_Efficient_process,
-        ]
-
-        for proc in process:
-            proc.start()
-
-        for proc in process:
-            proc.join()
-
-        run_times_Kruskal.append(
-            run_times_Kruskal_1
-            + run_times_Kruskal_2
-            + run_times_Kruskal_3
-            + run_times_Kruskal_4
-        )
-        MSTs_Weights_Kruskal.append(
-            MSTs_Weights_Kruskal_1
-            + MSTs_Weights_Kruskal_2
-            + MSTs_Weights_Kruskal_3
-            + MSTs_Weights_Kruskal_4
-        )
-        ########################################
     saving_data_Prim = []
     for i in range(len(Graphs)):
         saving_data_Prim.append((run_times_Prim[i], MSTs_Weights_Prim[i]))
