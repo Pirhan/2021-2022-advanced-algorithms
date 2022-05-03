d20a64a87e6c6824a330ee372def6a0bd4181532
NaN <blackpaper1993@gmail.com>
2022-05-03 16:45:28 +0200

    cleaned a bit code of test.py, removed unused import

--- Lab2/test.py
+++ Lab2/test.py
@@ -2,20 +2,25 @@ from data_structures.GEO_graph import Graph_GEO
 import os
 import geopy.distance
 import math
+
+
 def main():
 
-
-    foldername = "tsp_dataset"
+    foldername: str = "tsp_dataset"
     for filename in sorted(os.listdir(foldername)):
         if filename == "burma14.tsp":
             Graph = Graph_GEO()
-            Graph.initialize_from_file(foldername + "/" + filename)   
+            Graph.initialize_from_file(foldername + "/" + filename)
 
     print(Graph.nodes.items())
     print("\n")
     print("Our distance =>\t", Graph.getDistance(1, 12))
     print("Stack Overflow solution: =>\t", Graph.getDistance_2(1, 12))
-    print("Geopy distance =>\t", geopy.distance.distance((16.47, 96.10), (21.52, 95.59)).km)
+    print(
+        "Geopy distance =>\t",
+        geopy.distance.distance((16.47, 96.10), (21.52, 95.59)).km,
+    )
+
 
 if __name__ == "__main__":
     main()
