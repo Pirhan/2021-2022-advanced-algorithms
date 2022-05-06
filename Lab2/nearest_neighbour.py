aead8e41afb6464b4b488fbdac6e099bbd750a3a
NaN <blackpaper1993@gmail.com>
2022-05-06 11:10:29 +0200

    fixed comment in nearest_neighbour

--- Lab2/nearest_neighbour.py
+++ Lab2/nearest_neighbour.py
@@ -1,6 +1,4 @@
-from data_structures.Complete_graphs import * # type: ignore
-
-#  from data_structures.GEO_graph import GEO_graph  # type: ignore
+from data_structures.Complete_graphs import *  # type: ignore
 
 from typing import List
 
@@ -10,6 +8,8 @@ def computeSmallestNeighbour(
 ) -> int:  # return the index of the smallest_neighbour
     minimum_node: int = -1
     minimum: float = float("+Infinity")
+    #  TODO see if it is possible to precompute the distance between each node
+    #  at graph initialization
     for node in not_in_path:
         if node != current_pick and graph.getDistance(current_pick, node) < minimum:
 
@@ -18,6 +18,7 @@ def computeSmallestNeighbour(
     return minimum_node
 
 
+#  TODO collapse into nearestNeighbour, since it's a oneliner
 def removeNode(from_s: List[int], what: int) -> None:
     """ remove the neighbour to the set of the element in te path. this is done with side effects"""
     from_s.remove(what)
@@ -29,12 +30,11 @@ def nearestNeighbour(graph: CompleteGraph) -> List[int]:
         graph.getNodes()
     )  # keyview not indexable -> convert it into list
     current_pick: int = all_nodes[0]  # initialization, first selection
-    print("current pick", current_pick)
     not_in_path: List[int] = all_nodes[1:]
     final_path: List[int] = [current_pick]
     while (
         len(not_in_path) > 0
-    ):  # inside this loop, end when there are no more in the graph
+    ):  # inside this loop, end when there are no more nodes to add to the path
         smallest_neighbour: int = computeSmallestNeighbour(
             graph=graph, not_in_path=not_in_path, current_pick=current_pick
         )  # selection
@@ -43,9 +43,10 @@ def nearestNeighbour(graph: CompleteGraph) -> List[int]:
         ]  # insertion, last element is the new element of the path
         removeNode(
             from_s=not_in_path, what=smallest_neighbour
-        )  # side effects here, not_in_path has one element less(smallest_neighbour) in_path has one more element(smallest_neighbour)
+        )  # side effects here, not_in_path has one element less(smallest_neighbour
         current_pick = smallest_neighbour  # update the current_pick to reflect the path after the update
 
+    # following some simple testing function
     # sorted_final_path = final_path.sort()
     # print("f path", sorted_final_path)
     # print("len", len(final_path))
