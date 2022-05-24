c8f53ee5fe9d1d60d7f763e4128f5c28ddd69c4b
Giuseppe <giuseppe.rossano95@gmail.com>
2022-05-24 11:53:45 +0200

    Resolved some issues in Karger

--- Lab3/Karger.py
+++ Lab3/Karger.py
@@ -8,7 +8,7 @@ from data_structures.graph import Graph
 
 def Random_Select(C: List[float]):
 
-    r = np.random.randint(C[- 1])
+    r = np.random.randint(0, C[- 1])
     
     edge_found_index = binarySearch(r, C)   # This is the edge founded, achived as just an index
     
@@ -35,22 +35,15 @@ def binarySearch(r : int, C: List[float]):
         return None 
 
 def Edge_Select(G) -> Tuple[int,int]:
-
-    def comulative_weights(D: List[float]):
-        Comulative : List[int] = []
-        n_nodes = len(D)
-        
-        # Supposing that k can not be more than n_nodes
-        for i in range (n_nodes):
-            Comulative.append(sum(D[:i+1])) # Starting from 0
-        return Comulative
     
     (D,W) = G
-    C_D = comulative_weights(D)
+    C_D = [sum(D[:i]) for i in range (1,len(D)+1)] # Creating the comulative weight on D[i]
     print("C_D: \t", C_D[-1])
     u = Random_Select(C_D)
-    C_W = comulative_weights(W[u-1])     # To check 
-    print("C_W: \t", C_W[-1])
+
+    C_W = [sum((W[u-1])[:i]) for i in range (1,len(W[u-1])+1)]    # Creating the comulative weights on W[u-1]
+    print("C_W: \t", C_W[-1], "\t", u)
+    if C_W[-1] == 0: print("\n",C_W)
     v = Random_Select(C_W)
 
     return (u,v)
@@ -62,7 +55,7 @@ def Contract_Edge(G, u : int, v: int):
     W[u-1, v-1] = 0
     W[v-1, u-1] = 0
 
-    temp_nodes = [n for n in range(len(D)) if (n != u and n != v)]
+    temp_nodes = [n for n in range(1,len(D)+1) if (n != u or n != v)]
     #print("tn \t", temp_nodes)
     for w in temp_nodes:
         W[u-1, w-1] += W[v-1, w-1]
@@ -89,15 +82,16 @@ def Recursive_Contract(G):
 
     t = math.ceil(n_nodes/math.sqrt(2)+1)
     list_w = []
-    for _ in range(2):
-        newG = Contract((D,W), t)
+    for i in range(2):
+        print("Hi: ", i)
+        newG = Contract((D.copy(),W.copy()), t)
         list_w.append(Recursive_Contract(newG))
     return min(list_w)
 
 
 def Karger(G: Graph, k : int)-> float:
     minimumDistance = float("+Infinity")  # Useful for the comparison
-    for i in range(k):
+    for _ in range(k):
         t = Recursive_Contract((G.getD().copy(), G.getW().copy()))
         if t < minimumDistance :
             minimumDistance = t
