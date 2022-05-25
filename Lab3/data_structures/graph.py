d26c5758789adf1fe02632ec0fe68f8245b8deec
NaN <blackpaper1993@gmail.com>
2022-05-25 16:39:41 +0200

    incomplete version of stoerWagner

--- Lab3/data_structures/graph.py
+++ Lab3/data_structures/graph.py
@@ -1,4 +1,5 @@
-from typing import *
+# from typing import *
+from typing import List, Set, Tuple
 import numpy as np
 
 
@@ -16,6 +17,63 @@ class Graph:
         self.W[node1 - 1, node2 - 1] = weight
         self.W[node2 - 1, node1 - 1] = weight
 
+    #  get weight of a pair of nodes, required by stoer wagner
+    def getWeight(self, node1: int, node2: int) -> int:
+        return self.W[node1 - 1, node2 - 1]
+
+    def cutWeight(self, cut1: List[int]) -> int:
+        # returns the weight of the cut
+        # since the set of nodes is splitted in 2
+        # cut1 is enough to compute the nodes belonging to each cut
+        # ie cut1 will contain the nodes of the first partition, where all other nodes will be on the second partition
+        # idea:
+        # get the array weight for all nodes in cut1
+        # it will be an array of arrays
+        # for each of these nested array
+        # compute the sum of the weight only for those vertex which does not belong to cut1
+        # return the sum of this partial sum
+        # nodesInCut contains only the list of nodes weight of the first cut
+        # sum(singleElement for singleElement in container if singleElement not in container)
+        partialSum: int = 0
+        for nodesInCut in [x for ind, x in enumerate(self.getW()) if (ind + 1) in cut1]:
+            for nodeInCut in [
+                x for ind, x in enumerate(nodesInCut) if (ind + 1) not in cut1
+            ]:
+                partialSum += nodeInCut
+        return partialSum
+
+    def adjacentNodes(self, node: int) -> List[int]:
+        # get the row of nodes adjacent to node
+        # it will contain only the weights
+        # associate for each weight of a node it's index
+        # return only nodes where the weight is greater then 0
+        rowEdges: List[int] = self.getRowEdges(node=node)
+        return [vertex + 1 for vertex, weight in enumerate(rowEdges) if weight > 0]
+        # adds one just because vertex index seem to start from 1 instead of 0
+
+    def removeNodes(self, nodes: List[int]) -> None:
+        # remove nodes which does not belong to
+        # the graph anymore
+        # required by GlobalMiniumCut when computing
+        # the second cut
+        # remove from node
+        self.nodes = set([x for x in self.nodes if x + 1 not in nodes])
+        # remove edges where nodes are involved
+        self.edges = set(
+            [x for x in self.edges if not (x[0] + 1 in nodes or x[1] + 1 in nodes)]
+        )
+        # remove from weight matrix
+        # for each node which contains a node in nodes
+        # must set it's weight to zero
+        # remove from weight nodes
+        index: int = 0
+        while index < len(self.D) - 1:
+            if index + 1 in nodes:
+                self.D[index] = 0.0
+            index += 1
+
+        pass
+
     def getEdgesList(self):
         return self.edges
 
@@ -25,7 +83,7 @@ class Graph:
             D.append(self.getRowWeight(node))
         self.D = D
 
-    def getRowEdges(self, node: int) -> int:
+    def getRowEdges(self, node: int) -> List[int]:  # int:
         # Returns the array corresponding to the matrix row
         return self.W[node - 1]
 
@@ -56,7 +114,10 @@ class Graph:
             for line in lines[1:]:
                 node_1 = int(line.split()[0])
                 node_2 = int(line.split()[1])
-                weight = float(line.split()[2])
+                # weight = float(line.split()[2])
+                weight = int(
+                    line.split()[2]
+                )  # all weights seem to be int in the dataset
 
                 graph.addEdge(node_1, node_2, weight)
 
--- Lab3/stoerWagner.py
+++ Lab3/stoerWagner.py
@@ -1,33 +1,52 @@
 from typing import Tuple, List, Optional
-from data_structures import graph
+from data_structures.graph import Graph
+from data_structures.maxheap import maxHeap
 
 
-def globalMinimumCut(graph) -> Tuple[List[int], List[int]]:
+def globalMinimumCut(graph: Graph) -> Tuple[List[int], List[int]]:
     # where first component are vertex,
     # second component are edges
     # third component is the weight function
-    if len(graph[0]) == 2:
-        return (list(graph[0][0]), list(graph[0][1]))
+    nodes: List[int] = graph.getNodes()
+    if len(nodes) == 2:
+        return ([nodes[0]], [nodes[1]])
     else:
-        return ([], [])
+        (C1, s, t) = stMinimumCut(graph=graph)
+        C2 = globalMinimumCut(graph=graph)  # G minus s and t
+        # so we need to update the graph in some way
+        # remove the s t nodes
+        # and remove all the edges which connects to them?
+        if graph.cutWeight(C1) <= graph.cutWeight(C2):
+            return C1
+        else:
+            return C2
 
 
-def stMinimumCut(graph) -> Tuple[List[int], int, int, int]:
+def stMinimumCut(graph: Graph) -> Tuple[Tuple[List[int], List[int]], int, int]:
     # where first component are vertex,
     # second component are edges
     # third component is the weight function
-    priorityQueue: List[Tuple[int, int]] = []  # empty list at the beginning
+    priorityQueue = maxHeap()  # empty list at the beginning
     for vertex in graph[0]:
         vertexKey: int = 0
-        heappush(priorityQueue, (vertex, vertexKey))
+        priorityQueue.heappush(priorityQueue, (vertex, vertexKey))
     s: Optional[int] = None
     t: Optional[int] = None
-    while len(priorityQueue) > 0:
-        u = extractMax(priorityQueue)
+    while not priorityQueue.isEmpty():
+        u: int = priorityQueue.pop()[
+            0
+        ]  # recall that the vertex in the priorityQueue is provided as the first vertex also u is the vertex with maximum weight
         s = t
         t = u
-        for adjacent in range(10):
-            if adjacent in priorityQueue:
-                newKey = adjacent[key] + weight(u, adjacent)
-                IncreaseKey(priorityQueue, adjacent, newKey)
-    return None
+        for adjacent in graph.adjacentNodes(node=u):
+            if priorityQueue.findVertex(vertex=adjacent):
+                weightUV: int = graph.getWeight(node1=u, node2=adjacent)
+                assert weightUV > 0
+                priorityQueue.increaseKey(vertex=adjacent, weightToAdd=weightUV)
+
+    firstPartition: List[int] = graph.getNodes()
+    #  if for some reason s and t are underfined
+    #  raise some error
+    assert s is not None and t is not None
+    firstPartition.remove(t)
+    return ((firstPartition, [t]), s, t)
